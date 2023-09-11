68b3d758efa80e87c6b8d81836de1d42754be199
dyingsleeper <nik.savkin2004@mail.ru>
2023-09-12 01:30:10 +0300

    first commit

---
+++ LAB1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> arr;
+vector<int> d;
+int n, k;
+
+void printArray(vector<int> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+bool permission(int i)
+{
+    if ((arr[i] == k - 1 and d[i] == 1) or (arr[i] == 0 and d[i] == -1))
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int i, j;
+    cout << "n: ";
+    cin >> n;
+    cout << "k: ";
+    cin >> k;
+    arr.resize(n);
+    d.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = 0;
+        d[i] = 1;
+    }
+    while (true)
+    {
+        printArray(arr);
+        for (i = 0; i < n and permission(i); i++)
+            ;
+        if (i == n)
+            break;
+        arr[i] += d[i];
+
+        for (j = i - 1; j >= 0; j--)
+        {
+            d[j] = -d[j];
+        }
+    }
+}
